a5f54b9f0b5e4209adec333e8b7413e4008ea415
dialgamew <77679123+dialgamew@users.noreply.github.com>
2022-09-06 18:10:42 +0600

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -3,7 +3,7 @@ from bot import CMD_INDEX
 
 class _BotCommands:
     def __init__(self):
-        self.StartCommand = f'start{CMD_INDEX}'
+        self.StartCommand = f'start'
         self.MirrorCommand = (f'mirror{CMD_INDEX}', f'm{CMD_INDEX}')
         self.UnzipMirrorCommand = (f'unzipmirror{CMD_INDEX}', f'uzm{CMD_INDEX}')
         self.ZipMirrorCommand = (f'zipmirror{CMD_INDEX}', f'zm{CMD_INDEX}')
@@ -25,27 +25,27 @@ class _BotCommands:
         self.DeleteCommand = f'del{CMD_INDEX}'
         self.CancelMirror = f'cancel{CMD_INDEX}'
         self.CancelAllCommand = f'cancelall{CMD_INDEX}'
-        self.ListCommand = f'list{CMD_INDEX}'
+        self.ListCommand = f'list'
         self.SearchCommand = f'search{CMD_INDEX}'
-        self.StatusCommand = f'status{CMD_INDEX}'
-        self.AuthorizedUsersCommand = f'users{CMD_INDEX}'
-        self.AuthorizeCommand = f'authorize{CMD_INDEX}'
-        self.UnAuthorizeCommand = f'unauthorize{CMD_INDEX}'
-        self.AddSudoCommand = f'addsudo{CMD_INDEX}'
-        self.RmSudoCommand = f'rmsudo{CMD_INDEX}'
-        self.PingCommand = f'ping{CMD_INDEX}'
+        self.StatusCommand = f'status'
+        self.AuthorizedUsersCommand = f'users'
+        self.AuthorizeCommand = f'auth'
+        self.UnAuthorizeCommand = f'unauth'
+        self.AddSudoCommand = f'addsudo'
+        self.RmSudoCommand = f'rmsudo'
+        self.PingCommand = f'ping'
         self.RestartCommand = f'restart{CMD_INDEX}'
-        self.StatsCommand = f'stats{CMD_INDEX}'
-        self.HelpCommand = f'help{CMD_INDEX}'
-        self.LogCommand = f'log{CMD_INDEX}'
+        self.StatsCommand = f'stats'
+        self.HelpCommand = f'help'
+        self.LogCommand = f'log'
         self.ShellCommand = f'shell{CMD_INDEX}'
         self.EvalCommand = f'eval{CMD_INDEX}'
         self.ExecCommand = f'exec{CMD_INDEX}'
         self.ClearLocalsCommand = f'clearlocals{CMD_INDEX}'
-        self.LeechSetCommand = f'leechset{CMD_INDEX}'
-        self.SetThumbCommand = f'setthumb{CMD_INDEX}'
-        self.BtSelectCommand = f'btsel{CMD_INDEX}'
-        self.RssListCommand = (f'rsslist{CMD_INDEX}', f'rl{CMD_INDEX}')
+        self.LeechSetCommand = f'leechset'
+        self.SetThumbCommand = f'setthumb'
+        self.BtSelectCommand = f'btsel'
+        self.RssListCommand = (f'rsslist', f'rl')
         self.RssGetCommand = (f'rssget{CMD_INDEX}', f'rg{CMD_INDEX}')
         self.RssSubCommand = (f'rsssub{CMD_INDEX}', f'rs{CMD_INDEX}')
         self.RssUnSubCommand = (f'rssunsub{CMD_INDEX}', f'rus{CMD_INDEX}')
